4c6263e96e19f18f3077e8c6854be79a5c62a60d
Shreya Singh <shreya.official.2.25@gmail.com>
2025-03-19 06:52:43 +0530

    Step 1 / Lec 1 / 1_Basic_I_O

---
+++ Step 1  Learn the basics/Lec 1 Things to Know in C++JavaPython or any language/1_Basic_I_O.cpp
@@ -0,0 +1,53 @@
+// Hey Striver
+// Hey Striver
+// Hey
+// Hey Striver
+
+// #include<iostream>
+// int main(){
+//     std::cout<<"Hey Striver"<<"\n";     // \n is faster than endl
+//     std::cout<<"Hey Striver"<<std::endl<<"Hey"<<std::endl;
+//     std::cout<<"Hey Striver";
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hey Striver"<<"\n";
+//     cout<<"Hey Striver"<<endl<<"Hey"<<endl;
+//     cout<<"Hey Striver";
+//     return 0;
+// }
+
+
+
+// Enter number: 4
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cout<<"Enter number: ";
+//     cin>>x;
+//     return 0;
+// }
+
+
+
+// Enter number: 4 5
+// Value of x: 4
+// Value of y: 5
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x, y;
+    cout<<"Enter number: ";
+    cin>>x>>y;
+    cout<<"Value of x: "<<x<<"\nValue of y: "<<y;
+    return 0;
+}
+
+
+// #include<bits/stdc++.h>  // but it take bit more (negligible) time as it contains all the libraries inside this directory
