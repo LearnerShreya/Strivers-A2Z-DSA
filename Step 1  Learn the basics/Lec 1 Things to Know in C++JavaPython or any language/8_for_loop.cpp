74d91659600480d865007f8a8ff6c72c7ac45c6d
Shreya Singh <shreya.official.2.25@gmail.com>
2025-03-31 11:25:46 +0530

    8_for_loop.cpp

---
+++ Step 1  Learn the basics/Lec 1 Things to Know in C++JavaPython or any language/8_for_loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    for(int i=1; i<=5; i++){
+        cout<<"Striver "<<i<<endl;
+    }
+    cout<<endl;
+    
+    for(int j=5; j>=1; j--){
+        cout<<"Striver "<<j<<endl;
+    }
+}
